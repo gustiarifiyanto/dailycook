7c975f32b67e56a628f1a74c147492f0be70d531
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2026-06-19 22:07:45 +0700

    Menambahkan isi dari file models

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Ingredient extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+    ];
 }
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Recipe extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'url_file',
+        'url_video',
+        'category_id',
+        'recipe_author_id',
+    ];
 }
