9ab94168e4eb01ee8d2bc7d50345f22e27eee277
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2026-06-21 00:01:04 +0700

    Membuat controller dan API

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CategoryResource;
+use App\Models\Category;
+
+
+
+
+class CategoryController extends Controller
+{
+    //
+    public function index()
+    {
+        $categories = Category::withCount('recipes')->get();
+        return CategoryResource::collection($categories);
+    }
+
+    public function show(Category $category)
+    {
+        $category->load('recipes');
+        return new CategoryResource($category);
+    }
+}
---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'mydialy@cook.com',
+            'password' => Hash::make('123456789'), // ganti sesuai mau kamu
+        ]);
+    }
+}
